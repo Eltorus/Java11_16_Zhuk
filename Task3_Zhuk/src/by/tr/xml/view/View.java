b84dd6d078f18546abcc276fabb9690e3dc3d131
Eltorus <zhuk.pav96@gmail.com>
2017-01-20 15:34:23 +0200

    Task3

---
+++ Task3_Zhuk/src/by/tr/xml/view/View.java
@@ -0,0 +1,31 @@
+package by.tr.xml.view;
+
+import by.tr.xml.Service.Analyzer;
+import by.tr.xml.Service.exception.ServiceException;
+import by.tr.xml.Service.factory.AnalyzerFactory;
+import by.tr.xml.bean.NodeInfo;
+
+public class View {
+	public static void main(String[] args) {
+		String filename1 = "resource/breakfastMenu.xml";
+		String filename2 = "resource/notes.xml";
+		AnalyzerFactory analyzerFactory = AnalyzerFactory.getInstance();
+		Analyzer a = analyzerFactory.getAnalyzer();
+		try {
+			a.setFile(filename1);
+			NodeInfo info = null;
+			while ((info = a.next()) != null) {
+				System.out.println(info);
+			}
+			System.out.println("\n");
+			a.setFile(filename2);
+			info = null;
+			while ((info = a.next()) != null) {
+				System.out.println(info);
+			}
+			a.close();
+		} catch (ServiceException e) {
+			System.out.println(e.getMessage());
+		}
+	}
+}
